1f8b1d569b8f4c224bc98ccc44f8d4dcb08a1354
Shape <siruichou@gmail.com>
2020-05-20 22:26:56 +0800

    Add files via upload

---
+++ player/js/main.js
@@ -0,0 +1,88 @@
+
+var app = new Vue({
+  el: "#player",
+  data: {
+    // 查询关键字
+    query: "",
+    // 歌曲数组
+    musicList: [],
+    // 歌曲地址
+    musicUrl: "",
+    // 歌曲封面
+    musicCover: "",
+    // 歌曲评论
+    hotComments: [],
+    // 动画播放状态
+    isPlaying: false,
+    // 遮罩层的显示状态
+    isShow: false,
+    // mv地址
+    mvUrl: ""
+  },
+  methods: {
+    searchMusic: function() {
+      var that = this;
+      axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
+        function(response) {
+          // console.log(response);
+          that.musicList = response.data.result.songs;
+          console.log(response.data.result.songs);
+        },
+        function(err) {}
+      );
+    },
+    playMusic: function(musicId) {
+      //   console.log(musicId);
+      var that = this;
+      axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
+        function(response) {
+          // console.log(response);
+          // console.log(response.data.data[0].url);
+          that.musicUrl = response.data.data[0].url;
+        },
+        function(err) {}
+      );
+
+      axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
+        function(response) {
+          // console.log(response);
+          // console.log(response.data.songs[0].al.picUrl);
+          that.musicCover = response.data.songs[0].al.picUrl;
+        },
+        function(err) {}
+      );
+
+      axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
+        function(response) {
+          // console.log(response);
+          // console.log(response.data.hotComments);
+          that.hotComments = response.data.hotComments;
+        },
+        function(err) {}
+      );
+    },
+    play: function() {
+      // console.log("play");
+      this.isPlaying = true;
+    },
+    pause: function() {
+      // console.log("pause");
+      this.isPlaying = false;
+    },
+    playMV: function(mvid) {
+      var that = this;
+      axios.get("https://autumnfish.cn/mv/url?id=" + mvid).then(
+        function(response) {
+          // console.log(response);
+          console.log(response.data.data.url);
+          that.isShow = true;
+          that.mvUrl = response.data.data.url;
+        },
+        function(err) {}
+      );
+    },
+    hide: function() {
+      this.isShow = false;
+    }
+  }
+});
